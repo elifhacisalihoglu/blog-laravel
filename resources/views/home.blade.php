ecec01b015d1a5120fb3b0b8112adac56ce29110
Elif Hacısalihoğlu <hacisalihoglu.elif@gmail.com>
2021-01-09 21:49:48 +0300

    move links to layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,6 @@
 @section('title', 'Home')
 
 @section('content')
-    <ul>
-        <li><a href="{{ route('home') }}">Home</a>  </li>
-        <li><a href="{{ route('about') }}">About</a>  </li>
-        <li><a href="{{ route('blog') }}">Blog</a>  </li>
-        <li><a href="{{ route('contact') }}">Contact</a>  </li>
-    </ul>
 
     <p>This is my body content.</p>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,16 @@
         <title> @yield('title') | Blog - Laravel </title>
     </head>
     <body>
- 
+
+        <header> 
+            <ul>
+                <li><a href="{{ route('home') }}">Home</a>  </li>
+                <li><a href="{{ route('about') }}">About</a>  </li>
+                <li><a href="{{ route('blog') }}">Blog</a>  </li>
+                <li><a href="{{ route('contact') }}">Contact</a>  </li>
+            </ul>
+        </header> 
+        
         <div class="container">
             @yield('content')
         </div>
