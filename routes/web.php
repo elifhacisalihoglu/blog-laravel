ae146c3f7ca339bf10fbc89d321489abd10e5962
Elif Hacısalihoğlu <hacisalihoglu.elif@gmail.com>
2021-01-12 23:05:41 +0300

    Add Project Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProjectController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,9 @@ use App\Http\Controllers\PageController;
 */
 
 Route::get('/', [PageController::class, 'home'])->name('home');
-Route::get('/about', [PageController::class, 'about'])->name('about');;
-Route::get('/blog', [PageController::class, 'blog'])->name('blog');;
-Route::get('/contact', [PageController::class, 'contact'])->name('contact');;
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/blog', [PageController::class, 'blog'])->name('blog');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
+Route::resource('projects', ProjectController::class);
+
